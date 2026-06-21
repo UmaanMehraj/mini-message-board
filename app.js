6801c712e5fa119c67a55c71d04b9fc9db9b0adf
UmaanMehraj <umaanmehraj5@gmail.com>
2026-06-21 13:31:02 +0530

    Add: routes, views

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const path = require('node:path')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+const PORT = 3000
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+app.get('/', (req, res) => {
+    res.render('index', { messages: messages })
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err
+    }
+})
