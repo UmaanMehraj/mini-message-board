5a4e20596956e9cb0af1eab68a255214b1911bcb
UmaanMehraj <umaanmehraj5@gmail.com>
2026-06-23 19:13:06 +0530

    Add: form.ejs, new post & get router to indexRouter.js to handle post requests for new messages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const indexRouter = require('./routes/indexRouter')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = 3000
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,4 +20,13 @@ indexRouter.get('/', (req, res) => {
     res.render('index', { messages: messages })
 })
 
+indexRouter.get('/new', (req, res) => {
+    res.render('form')
+})
+
+indexRouter.post('/new', (req, res) => {
+    messages.push({ text: req.body.message, user: req.body.authorName, added: new Date() })
+    res.redirect('/')
+})
+
 module.exports = indexRouter
