d8a792c42b43d68b7f108ee705c28a1fe67e214d
UmaanMehraj <umaanmehraj5@gmail.com>
2026-08-24 21:59:06 +0530

    done commit

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,20 @@
-const { Pool } = require('pg')
+require('dotenv').config();
 
-module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
+const { Pool } = require('pg');
+
+const url = new URL(process.env.DATABASE_URL);
+
+const pool = new Pool({
+    user: decodeURIComponent(url.username),
+    password: decodeURIComponent(url.password),
+    host: url.hostname,
+    port: 5432,
+    database: url.pathname.slice(1),
+    ssl: {
+        require: true
+    }
+});
+
+console.log('pool created');
+
+module.exports = pool;
--- db/populatedb.js
+++ db/populatedb.js
@@ -1,19 +1,36 @@
-const { Client } = require('pg')
+require('dotenv').config();
+
+const { Client } = require('pg');
 
 const SQL = `
-INSERT INTO messages (text, user) VALUES ('Hi', 'Orlando'), ('Hello', 'Bloom'), ('Not all treasure is silver and gold mate!', 'Jack Sparrow')`
+INSERT INTO messages (text, user)
+VALUES
+    ('Hi', 'Orlando'),
+    ('Hello', 'Bloom'),
+    ('Not all treasure is silver and gold mate!', 'Jack Sparrow')
+`;
 
 async function main() {
-    console.log('seeding...')
-    const client = new Client({
-        connectionString: process.env.DB_CONNECTION_STRING
-    })
+    console.log('seeding...');
 
-    await client.connect()
-    await client.query(SQL)
-    await client.end()
-    console.log('done')
+    const url = new URL(process.env.DATABASE_URL);
+
+    const client = new Client({
+        user: decodeURIComponent(url.username),
+        password: decodeURIComponent(url.password),
+        host: url.hostname,
+        port: 5432,
+        database: url.pathname.slice(1),
+        ssl: {
+            require: true
+        }
+    });
+
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+
+    console.log('done');
 }
 
-
-main()
+main().catch(console.error);
